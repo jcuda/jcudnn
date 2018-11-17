ce71f2fc02817cecace51a80e6db5f0c7f10cffc
Marco Hutter <jcuda@jcuda.org>
2018-11-17 16:10:12 +0100

    Added missing files from update to cuDNN 7.4.1

---
+++ JCudnnJava/src/main/java/jcuda/jcudnn/cudnnRNNClipMode.java
@@ -0,0 +1,61 @@
+/*
+ * JCudnn - Java bindings for cuDNN, the NVIDIA CUDA
+ * Deep Neural Network library, to be used with JCuda
+ *
+ * Copyright (c) 2015-2018 Marco Hutter - http://www.jcuda.org
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package jcuda.jcudnn;
+
+/**
+* CUDNN clipping mode type
+*/
+public class cudnnRNNClipMode
+{
+    public static final int CUDNN_RNN_CLIP_NONE = 0;
+    public static final int CUDNN_RNN_CLIP_MINMAX = 1;
+
+    /**
+     * Private constructor to prevent instantiation
+     */
+    private cudnnRNNClipMode()
+    {
+        // Private constructor to prevent instantiation
+    }
+
+    /**
+     * Returns a string representation of the given constant
+     *
+     * @return A string representation of the given constant
+     */
+    public static String stringFor(int n)
+    {
+        switch (n)
+        {
+            case CUDNN_RNN_CLIP_NONE: return "CUDNN_RNN_CLIP_NONE";
+            case CUDNN_RNN_CLIP_MINMAX: return "CUDNN_RNN_CLIP_MINMAX";
+        }
+        return "INVALID cudnnRNNClipMode: "+n;
+    }
+}
+
---
+++ JCudnnJava/src/main/java/jcuda/jcudnn/cudnnRNNDataLayout.java
@@ -0,0 +1,63 @@
+/*
+ * JCudnn - Java bindings for cuDNN, the NVIDIA CUDA
+ * Deep Neural Network library, to be used with JCuda
+ *
+ * Copyright (c) 2015-2018 Marco Hutter - http://www.jcuda.org
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package jcuda.jcudnn;
+
+public class cudnnRNNDataLayout
+{
+    public static final int CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_UNPACKED = 0;
+    /**
+     * imples sequences are sorted 
+     */
+    public static final int CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_PACKED = 1;
+    public static final int CUDNN_RNN_DATA_LAYOUT_BATCH_MAJOR_UNPACKED = 2;
+
+    /**
+     * Private constructor to prevent instantiation
+     */
+    private cudnnRNNDataLayout()
+    {
+        // Private constructor to prevent instantiation
+    }
+
+    /**
+     * Returns a string representation of the given constant
+     *
+     * @return A string representation of the given constant
+     */
+    public static String stringFor(int n)
+    {
+        switch (n)
+        {
+            case CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_UNPACKED: return "CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_UNPACKED";
+            case CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_PACKED: return "CUDNN_RNN_DATA_LAYOUT_SEQ_MAJOR_PACKED";
+            case CUDNN_RNN_DATA_LAYOUT_BATCH_MAJOR_UNPACKED: return "CUDNN_RNN_DATA_LAYOUT_BATCH_MAJOR_UNPACKED";
+        }
+        return "INVALID cudnnRNNDataLayout: "+n;
+    }
+}
+
